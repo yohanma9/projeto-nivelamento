3e458a50fa0b132c90651a58ce1c3ce25b107695
Yohan Alexander <yohan.franca@pagueon.com.br>
2020-08-06 16:51:16 -0300

    🎨 aninhamento das rotas na API

--- api/urls.py
+++ api/urls.py
@@ -1,22 +1,33 @@
-from django.urls import path
-from django.urls import include
-from rest_framework import routers
+from django.urls import include, path
+
+from rest_framework_nested import routers
 from rest_framework_simplejwt import views as jwt_views
 
-from .views import EnderecoViewSet
-from .views import ClienteViewSet
-from .views import ProdutoViewSet
-from .views import PedidoViewSet
+from .views import (ClienteViewSet, EnderecoViewSet, PedidoViewSet,
+                    ProdutosViewSet, ProdutoViewSet)
 
-router = routers.DefaultRouter()
-router.register(r'enderecos', EnderecoViewSet)
-router.register(r'clientes', ClienteViewSet)
-router.register(r'produtos', ProdutoViewSet)
-router.register(r'pedidos', PedidoViewSet)
+router = routers.SimpleRouter()
+
+router.register(r"pedidos", PedidoViewSet)
+pedidos_router = routers.NestedSimpleRouter(router,
+                                            r"pedidos",
+                                            lookup="pedido")
+pedidos_router.register(r"produtos", ProdutoViewSet)
+
+router.register(r"clientes", ClienteViewSet)
+clientes_router = routers.NestedSimpleRouter(router,
+                                             r"clientes",
+                                             lookup="cliente")
+clientes_router.register(r"enderecos", EnderecoViewSet)
+
+router.register(r"produtos", ProdutosViewSet)
 
 urlpatterns = [
-    path('api/', include(router.urls), name='api'),
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token'),
-    path('api/token/refresh/',
-        jwt_views.TokenRefreshView.as_view(), name='refresh')
+    path("api/", include(router.urls), name="api"),
+    path("api/", include(pedidos_router.urls), name="api"),
+    path("api/", include(clientes_router.urls), name="api"),
+    path("api/token/", jwt_views.TokenObtainPairView.as_view(), name="token"),
+    path("api/token/refresh/",
+         jwt_views.TokenRefreshView.as_view(),
+         name="refresh"),
 ]
--- api/views.py
+++ api/views.py
@@ -20,12 +20,25 @@ class EnderecoViewSet(viewsets.ModelViewSet):
     serializer_class = EnderecoSerializer
     permission_classes = (permissions.IsAuthenticated, )
 
+    def get_queryset(self):
+        return Endereco.objects.filter(cliente=self.kwargs["cliente_pk"])
+
+
+class ProdutosViewSet(viewsets.ModelViewSet):
+
+    queryset = Produto.objects.all()
+    serializer_class = ProdutoSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
 
 class ProdutoViewSet(viewsets.ModelViewSet):
 
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
     permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+
+    def get_queryset(self):
+        return Produto.objects.filter(pedido=self.kwargs["pedido_pk"])
 
 
 class PedidoViewSet(viewsets.ModelViewSet):
